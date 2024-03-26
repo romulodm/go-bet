b50e41b77533e0fdf48c35e92ffb88e8384b8ddd
Romulo <romulotg12@gmail.com>
2024-03-26 15:38:50 -0300

    feat: adding function to spin the roulette wheel, the number to be chosen is received as a parameter

--- frontend/js/roulette-double.js
+++ frontend/js/roulette-double.js
@@ -1,3 +1,4 @@
+const betButton = document.getElementById('roulette-double-bet-button');
 
 function setupWheel() {
     var wheel = document.querySelector('.roulette-container .wheel'),
@@ -26,6 +27,42 @@ function setupWheel() {
     }
 }
 
+function spinWheel(roll) {
+    var wheel = document.querySelector('.roulette-container .wheel')
+    var order = [0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4]
+    var position = order.indexOf(roll);
+
+    var rows = 12
+    var card = 75 + 3 * 2
+    var landingPosition = (rows * 15 * card) + (position * card);
+
+    var randomize = Math.floor(Math.random() * 75) - (75 / 2);
+
+    landingPosition = landingPosition + randomize;
+
+    var object = {
+        x: Math.floor(Math.random() * 50) / 100,
+        y: Math.floor(Math.random() * 20) / 100
+    };
+
+    wheel.style.transitionTimingFunction = 'cubic-bezier(0,' + object.x + ',' + object.y + ',1)';
+    wheel.style.transitionDuration = '6s';
+    wheel.style.transform = 'translate3d(-' + landingPosition + 'px, 0px, 0px)';
+
+    setTimeout(function() {
+        wheel.style.transitionTimingFunction = '';
+        wheel.style.transitionDuration = '';
+
+        var resetTo = -(position * card + randomize);
+        wheel.style.transform = 'translate3d(' + resetTo + 'px, 0px, 0px)';
+    }, 6 * 1000);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     setupWheel();
+});
+
+betButton.addEventListener('click', function() {
+    console.log("oi");
+    spinWheel(2);
 });
