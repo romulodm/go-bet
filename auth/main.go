beb0aa5dd5056a8f99e6b51f31d41b40013ed682
Romulo <romulotg12@gmail.com>
2024-04-19 00:44:11 -0300

    feat: adding the main file that loads the configurations and creates gRPC and server.go instances

---
+++ auth/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net"
+
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/romulodm/go-bet/auth/config"
+	db "github.com/romulodm/go-bet/auth/database/sqlc"
+	"github.com/romulodm/go-bet/auth/jwt"
+	"github.com/romulodm/go-bet/auth/pb/github.com/romulodm/gobet/auth/pb"
+	"github.com/romulodm/go-bet/auth/server"
+)
+
+func main() {
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.GetDatabaseURL())
+	if err != nil {
+		log.Fatal("Error to connect DB:", err)
+	}
+
+	store := db.NewStore(conn)
+	jwtManager := jwt.NewManager(config.JWTSecretKey, config.JWTTokenExpiry)
+
+	server, err := server.NewServer(config, *store, *jwtManager)
+	if err != nil {
+		log.Fatal("Cannot create server:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterAuthenticationServiceServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("Cannot create gRPC listener:", err)
+	}
+
+	log.Printf("Starting gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("Cannot start gRPC server:", err)
+	}
+}
