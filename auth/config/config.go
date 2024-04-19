e73d2a0e94d7167f8633d2b54e81395187f57bb8
Romulo <romulotg12@gmail.com>
2024-04-18 23:25:19 -0300

    feat: adding jwt pass key, hwt duration and grpc address on config file

--- auth/config/config.go
+++ auth/config/config.go
@@ -4,17 +4,21 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	DBDriver   string
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
+	GRPCServerAddress string
+	DBDriver          string
+	DBHost            string
+	DBPort            string
+	DBUser            string
+	DBPassword        string
+	DBName            string
+	JWTSecretKey      []byte
+	JWTTokenExpiry    time.Duration
 }
 
 func LoadConfig() (config Config, err error) {
@@ -28,12 +32,21 @@ func LoadConfig() (config Config, err error) {
 		return config, fmt.Errorf("error on load enviroment variables: %v", err)
 	}
 
+	config.GRPCServerAddress = os.Getenv("GRPC_SERVER_ADDRESS")
 	config.DBDriver = os.Getenv("DB_DRIVER")
 	config.DBHost = os.Getenv("DB_HOST")
 	config.DBPort = os.Getenv("DB_PORT")
 	config.DBUser = os.Getenv("DB_USER")
 	config.DBPassword = os.Getenv("DB_PASSWORD")
 	config.DBName = os.Getenv("DB_NAME")
+	config.JWTSecretKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+
+	jwtDurationString := os.Getenv("JWT_DURATION")
+	jwtDuration, err := time.ParseDuration(jwtDurationString)
+	if err != nil {
+		return config, fmt.Errorf("error parsing JWT token expiry duration: %v", err)
+	}
+	config.JWTTokenExpiry = jwtDuration
 
 	return config, nil
 }
