a404ec04fdf7dc2e22ce082c0efa53cded5d2498
Romulo <romulotg12@gmail.com>
2024-04-25 19:58:35 -0300

    feat: adding error messages in multiple languages ​​to the authentication service

---
+++ auth/errs/errors.go
@@ -0,0 +1,65 @@
+package errs
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
+
+type ErrorMessages map[string]map[string]string
+
+var errorMessages ErrorMessages
+
+func init() {
+	err := loadErrorMessages()
+	if err != nil {
+		log.Fatalf("Error loading error messages: %v", err)
+	}
+}
+
+func loadErrorMessages() error {
+	filePath, err := filepath.Abs("errs/errors.json")
+	if err != nil {
+		return err
+	}
+
+	// Lê o conteúdo do arquivo JSON
+	fileContent, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	// Decodifica o JSON
+	var errorMessagesJSON map[string]map[string]string
+	if err := json.Unmarshal(fileContent, &errorMessagesJSON); err != nil {
+		return err
+	}
+
+	// Converter para o formato desejado (ErrorMessages)
+	errorMessages = make(ErrorMessages)
+	for k, v := range errorMessagesJSON {
+		errorMessages[k] = v
+	}
+
+	fmt.Println(errorMessages)
+
+	return nil
+}
+
+func GetErrorMessage(language string, errorCode string) string {
+	log.Printf("Buscando mensagem de erro para idioma %s e código de erro %s", language, errorCode)
+
+	if languageMessages, ok := errorMessages[language]; ok {
+		log.Printf("Mensagens disponíveis para o idioma %s: %v", language, languageMessages)
+
+		if message, ok := languageMessages[errorCode]; ok {
+			log.Printf("Mensagem encontrada para o código de erro %s: %s", errorCode, message)
+			return message
+		}
+	}
+
+	log.Printf("Mensagem de erro não encontrada para o idioma %s e código de erro %s", language, errorCode)
+	return "An error occurred."
+}
