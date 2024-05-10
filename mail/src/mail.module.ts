1a311036d7f70bf582417abc85ce1981a7665e76
Romulo <romulotg12@gmail.com>
2024-05-10 15:15:24 -0300

    fix: changing process.env to ConfigService and changing templates directory

--- mail/src/mail.module.ts
+++ mail/src/mail.module.ts
@@ -1,31 +1,35 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { AppConfigModule } from './app-config/app-config.module';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { join } from 'path';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    MailerModule.forRoot({
-      transport: {
-        host: String(process.env.MAIL_HOST),
-        port: Number(process.env.MAIL_PORT),
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS,
+    AppConfigModule,
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get<string>('MAIL_HOST'),
+          port: config.get<number>('MAIL_PORT'),
+          secure: false,
+          auth: {
+            user: config.get<string>('MAIL_USER'),
+            pass: config.get<string>('MAIL_PASS'),
+          },
         },
-      },
-      template: {
-        dir: join(__dirname, 'templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
+        template: {
+          dir: process.cwd() + '/templates/',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
         },
-      },
-    })
+      }),
+    }),    
   ],
   controllers: [MailController],
   providers: [MailService],
